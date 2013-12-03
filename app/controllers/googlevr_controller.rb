e2f62d96606b4307d6dbb75c0049586bca08d906
Jeff Hsu <jeff.hsu@west.cmu.edu>
2013-12-03 01:08:19 -0800

    finished post message

--- app/controllers/googlevr_controller.rb
+++ app/controllers/googlevr_controller.rb
@@ -1,11 +1,17 @@
 class GooglevrController < ApplicationController
-	protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json'  #Rails 4
+	protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json'}  #Rails 4
 	# skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	def create
 		@message = params[:message]
-		@response = {"message"=>@message}
-      	respond_to do |format|
-      	format.json {render :json => @response}
-      end
-    end
+    p @message
+
+    #render text: "Done!"
+
+    render json: {"message"=>"Successful!"}
+
+    #@response = {"message"=>"Successful!"}
+    #respond_to do |format|
+    #  	format.json {render :json => @response}
+    #end
+  end
 end

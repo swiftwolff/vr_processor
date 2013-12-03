78f8947d5140ee742357d5f76cb295fe544feef2
Jeff Hsu <jeff.hsu@west.cmu.edu>
2013-12-03 00:14:44 -0800

    adding skip filters for csrf

--- app/controllers/googlevr_controller.rb
+++ app/controllers/googlevr_controller.rb
@@ -1,8 +1,11 @@
 class GooglevrController < ApplicationController
+	protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json'  #Rails 4
+	# skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	def create
 		@message = params[:message]
 		@response = {"message"=>@message}
-    	respond_to do |format|
+      	respond_to do |format|
       	format.json {render :json => @response}
+      end
     end
 end
